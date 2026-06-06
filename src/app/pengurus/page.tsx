e30a1eb1355cf69bfde4307d5df8f6b028cf4d9a
kouki <145797585+Sepuh23@users.noreply.github.com>
2026-06-06 15:44:37 +0700

    Update page.tsx

--- src/app/pengurus/page.tsx
+++ src/app/pengurus/page.tsx
@@ -4,7 +4,7 @@ import React, { useMemo, useState } from 'react'
 import { type Pengurus } from '@/lib/supabase'
 import Image from 'next/image'
 import { Instagram, Search, ClipboardList } from 'lucide-react'
-
+import PengurusView from '@/components/PengurusView'
 // Definisikan tipe untuk struktur_jabatan
 interface StrukturJabatan {
   urutan: number
